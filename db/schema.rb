3a89ecd61755a6ce4cc739c2c2d1cdb591c8a0b0
Kevin Finley <kftwotwo@gmail.com>
2017-04-20 22:06:30 -0700

    BIl-28: add category model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421005158) do
+ActiveRecord::Schema.define(version: 20170421044302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 20170421005158) do
     t.integer  "user_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.integer "bill_id"
+    t.string  "kind"
   end
 
   create_table "companies", force: :cascade do |t|
