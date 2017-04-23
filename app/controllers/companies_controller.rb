5061ce54fae9cf426c12986fa2168bddbb80e1cf
Kevin Finley <kftwotwo@gmail.com>
2017-04-22 20:33:24 -0700

    BIL-29: add method to create a company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -9,4 +9,25 @@ class CompaniesController < ApplicationController
   def new
     @company = Company.new
   end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      flash[:success] = "Success!"
+      redirect_to companies_path
+    else
+      flash[:alert] = "Opps..Try Again"
+      render :new
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(
+      :user_id,
+      :name,
+      :website
+    )
+  end
 end
