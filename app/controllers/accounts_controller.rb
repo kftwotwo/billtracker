abd4d4658b3093e49e281efc224abc55fa607163
Kevin Finley <kftwotwo@gmail.com>
2017-04-24 18:33:31 -0700

    BIL-30: add method to create accounts

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,36 @@
+class AccountsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @account = Account.new
+    @company = Company.find(params[:company_id])
+  end
+
+  def create
+    @company = Company.find(params[:company_id])
+    @account = @company.accounts.new(account_params)
+    if @account.save
+      flash[:success] = "Success!"
+      redirect_to companies_path
+    else
+      flash[:alert] = "Opps..Try Again"
+      render :new
+    end
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(
+      :company_id,
+      :user_id,
+      :account_name,
+      :account_number,
+      :kind
+    )
+  end
+end
